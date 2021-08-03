f1720eec3f306b1a68fcd92f252d4a0712eae261
abelaraujo <abel2901@gmail.com>
2021-08-03 12:30:55 -0300

    Create classes to adapter and activity

--- app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
@@ -3,8 +3,14 @@ package com.br.ceeppoc.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.br.ceeppoc.R;
+import com.br.ceeppoc.dao.NotaDAO;
+import com.br.ceeppoc.model.Nota;
+import com.br.ceeppoc.ui.recyclerview.adapter.ListaNotasAdapter;
+
+import java.util.List;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +18,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listView);
+
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("primeira nota", "segunda nota"));
+
+        List<Nota> todaNotas = dao.todos();
+
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todaNotas));
     }
 }
---
+++ app/src/main/java/com/br/ceeppoc/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -0,0 +1,57 @@
+package com.br.ceeppoc.ui.recyclerview.adapter;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.br.ceeppoc.R;
+import com.br.ceeppoc.model.Nota;
+
+import java.util.List;
+
+public class ListaNotasAdapter extends BaseAdapter {
+    private final Context context;
+    private final List<Nota> notas;
+
+    public ListaNotasAdapter(Context context, List<Nota> notas) {
+        this.context = context;
+        this.notas = notas;
+    }
+
+    @Override
+    public int getCount() {
+        return notas.size();
+    }
+
+    @Override
+    public Nota getItem(int posicao) {
+        return notas.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
+        View viewCriada = LayoutInflater.from(context)
+                .inflate(R.layout.item_nota, viewGroup, false);
+        Nota nota = notas.get(posicao);
+
+        TextView titulo = viewCriada.findViewById(R.id.item_nota_titulo);
+        titulo.setText(nota.getTitulo());
+
+        TextView descricao = viewCriada.findViewById(R.id.item_nota_descricao);
+        descricao.setText(nota.getDescricao());
+
+        return viewCriada;
+    }
+}
