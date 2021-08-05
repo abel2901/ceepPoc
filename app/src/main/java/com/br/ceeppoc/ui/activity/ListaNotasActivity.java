8193e6e4f6ee9fe0ae9e7c9577ece72aedbb6bfe
abelaraujo <abel2901@gmail.com>
2021-08-05 19:05:33 -0300

    Add recycler view adapter

--- app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
@@ -1,13 +1,15 @@
 package com.br.ceeppoc.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.br.ceeppoc.R;
 import com.br.ceeppoc.dao.NotaDAO;
 import com.br.ceeppoc.model.Nota;
+import com.br.ceeppoc.ui.recyclerview.adapter.ListNotasAdapter;
 import com.br.ceeppoc.ui.recyclerview.adapter.ListaNotasAdapter;
 
 import java.util.List;
@@ -19,13 +21,17 @@ public class ListaNotasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
 
-        ListView listaNotas = findViewById(R.id.listView);
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
 
         NotaDAO dao = new NotaDAO();
-        dao.insere(new Nota("primeira nota", "segunda nota"));
+        for (int i = 0; i <= 1000; i++) {
+            dao.insere(new Nota("Titulo " + i, "descricao " + i));
+        }
 
         List<Nota> todaNotas = dao.todos();
 
-        listaNotas.setAdapter(new ListaNotasAdapter(this, todaNotas));
+        listaNotas.setAdapter(new ListNotasAdapter(this, todaNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
     }
 }
