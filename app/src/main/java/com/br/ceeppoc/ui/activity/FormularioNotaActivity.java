dacac1bd7843ac2e1c9a1511483d66950fe7a417
abelaraujo <abel2901@gmail.com>
2021-08-11 20:26:42 -0300

    create formulary activity

---
+++ app/src/main/java/com/br/ceeppoc/ui/activity/FormularioNotaActivity.java
@@ -0,0 +1,16 @@
+package com.br.ceeppoc.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.ceeppoc.R;
+
+public class FormularioNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_nota_activity);
+    }
+}
--- app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/br/ceeppoc/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package com.br.ceeppoc.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,7 +13,6 @@ import com.br.ceeppoc.R;
 import com.br.ceeppoc.dao.NotaDAO;
 import com.br.ceeppoc.model.Nota;
 import com.br.ceeppoc.ui.recyclerview.adapter.ListNotasAdapter;
-import com.br.ceeppoc.ui.recyclerview.adapter.ListaNotasAdapter;
 
 import java.util.List;
 
@@ -29,6 +31,17 @@ public class ListaNotasActivity extends AppCompatActivity {
         }
 
         List<Nota> todaNotas = dao.todos();
+
+        TextView insereNotaBotao = findViewById(R.id.lista_notas_insere_nota);
+        insereNotaBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iniciaFormularioNota =
+                        new Intent(ListaNotasActivity.this,
+                                FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
 
         listaNotas.setAdapter(new ListNotasAdapter(this, todaNotas));
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
